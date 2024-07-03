22f328b83a99077fbb02577ccab7a19c375a9050
Nemr <fresh.falafel.mtl@gmail.com>
2024-07-02 23:58:09 -0400

    first commit

---
+++ js/menu_items.js
@@ -0,0 +1,191 @@
+var menu_items = {
+    "items": [
+        {
+            "type": "falafel",
+            "name": "Assiette Falafel",
+            "image": "images/af3_200.png",
+            "description": "4 morceaux Falafel + Patate à L'ail + 4 ingrédients au choix + Sauce Ranch maison + Sauce à l'ail \
+                + Sauce Tahini + Cornichons et/ou Navet.",
+            "ingredients": "Ingrédients: Laitue, Concombre, Tomate, Mais, Oignon, Poivron.",
+            "cost": "$10",
+            "extras": [
+                {
+                    "description": "1 morceau Falafel",
+                    "cost": "$0.75"
+                },
+                {
+                    "description": "1 ingrédient de plus",
+                    "cost": "$1.00"
+                },
+                {
+                    "description": "1 boisson",
+                    "cost": "$1.50"
+                }
+            ]
+        },
+        {
+            "type": "crispy",
+            "name": "Assiette Crispy",
+            "image": "images/ac1_200.png",
+            "description": "3 morceaux Crispy + Patate à L'ail + 4 ingrédients au choix + Sauce Ranch maison + \
+                Sauce à l'ail + Cornichons et/ou Navet.",
+            "ingredients": "Ingrédients: Laitue, Concombre, Tomate, Mais, Oignon, Poivron.",
+            "cost": "$12",
+            "extras": [
+                {
+                    "description": "1 morceau Crispy",
+                    "cost": "$1.50"
+                },
+                {
+                    "description": "1 ingrédient de plus",
+                    "cost": "$1.00"
+                },
+                {
+                    "description": "1 boisson",
+                    "cost": "$1.50"
+                }
+            ]
+        },
+        {
+            "type": "falafel",
+            "name": "Pita Falafel",
+            "image": "images/sf_200.jpg",
+            "description": "\
+                Pita ...... $5.00<br>\
+                Pita + boisson ...... $6.00<br>\
+                2x Pita + boisson ...... $10.00<br>\
+                <br>\
+                Trios ...... $8.50<br>\
+                (Pita + boisson + votre choix:<br>\
+                Salade, Frite, ou Patate à l'ail)\
+                ",
+            "ingredients": "",
+            "cost": "$5",
+            "extras": [
+                
+            ]
+        },
+        {
+            "type": "crispy",
+            "name": "Wrap Crispy",
+            "image": "images/wc_200.jpg",
+            "description": "\
+                Wrap ...... $6.50<br>\
+                Wrap + boisson ...... $7.50<br>\
+                2x Wrap + boisson ...... $13.50<br>\
+                <br>\
+                Trios ...... $10.00<br>\
+                (Wrap + boisson + votre choix:<br>\
+                Salade, Salade de choux, Frite, ou Patate à l'ail)\
+                ",
+            "ingredients": "",
+            "cost": "$6.50",
+            "extras": [
+                
+            ]
+        },
+        {
+            "type": "falafel",
+            "name": "Morceaux Falafel",
+            "image": "images/df_200.png",
+            "description": "\
+                6 morceaux ...... $5.00<br>\
+                12 morceaux ...... $9.00<br>\
+                (Servis avec sauce Tahini)<br>\
+                ",
+            "ingredients": "",
+            "cost": "À partir de $5.00",
+            "extras": []
+        },
+        {
+            "type": "crispy",
+            "name": "Morceaux Crispy",
+            "image": "images/mc_200.png",
+            "description": "\
+                5 morceaux ...... $8.00<br>\
+                10 morceaux ...... $14.50<br>\
+                (Servis avec sauce à l'ail)<br>\
+                ",
+            "ingredients": "",
+            "cost": "À partir de $8.00",
+            "extras": []
+        },
+        {
+            "type": "extra",
+            "name": "Patates Frites",
+            "image": "images/frites_200.png",
+            "description": "\
+                Petit ...... $4.00<br>\
+                Grand ...... $8.00<br>\
+                ",
+            "ingredients": "",
+            "cost": "À partir de $4.00",
+            "extras": []
+        },
+        {
+            "type": "extra",
+            "name": "Patates à l'ail",
+            "image": "images/pa_200.png",
+            "description": "\
+                Petit ...... $4.00<br>\
+                Grand ...... $9.00<br>\
+                ",
+            "ingredients": "",
+            "cost": "À partir de $4.00",
+            "extras": []
+        },
+        {
+            "type": "extra",
+            "name": "Salade de Choux",
+            "image": "images/choux1_200.png",
+            "description": "\
+                Petit ...... $4.00<br>\
+                Grand ...... $8.00<br>\
+                ",
+            "ingredients": "",
+            "cost": "À partir de $4.00",
+            "extras": []
+        },
+        {
+            "type": "extra",
+            "name": "Salade Maison",
+            "image": "images/salade_200.png",
+            "description": "\
+                Avec sauce Ranch maison.<br>\
+                Petit ...... $4.50<br>\
+                Grand ...... $10.00<br>\
+                ",
+            "ingredients": "",
+            "cost": "À partir de $4.50",
+            "extras": []
+        },
+        {
+            "type": "boisson",
+            "name": "Boisson",
+            "image": "images/pc_cola_200.png",
+            "description": "\
+                PC Cola, PC Spritz, PC Ginger<br>\
+                PC Lemonade, Selection Orange, Eau<br>\
+                PC Soda Ginger<br>\
+                ",
+            "ingredients": "",
+            "cost": "$1.50",
+            "extras": []
+        },
+        {
+            "type": "extra",
+            "name": "Sauces",
+            "image": "images/sauces_200.png",
+            "description": "\
+                Sauce à l'ail (75ml) ...... $1.00<br>\
+                Sauce Tahini (75ml) ...... $1.00<br>\
+                Sauce Moutard et Miel (75ml) ...... $1.50<br>\
+                Sauce Cocktail (75ml) ...... $1.50<br>\
+                Sauce Ranch Maison (75ml) ...... $1.50<br>\
+                ",
+            "ingredients": "",
+            "cost": "À partir de $1.00",
+            "extras": []
+        }
+    ]
+}
